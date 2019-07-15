24a58b4128cc15cfc6347f907a0566b760b21345
Chaynor Hsiao <chaynsaw@gmail.com>
2019-07-14 21:56:17 -0700

    Implement pluckFirstLineFromFileAsync

--- exercises/bare_minimum/promiseConstructor.js
+++ exercises/bare_minimum/promiseConstructor.js
@@ -10,7 +10,16 @@ var Promise = require('bluebird');
 
 // This function should retrieve the first line of the file at `filePath`
 var pluckFirstLineFromFileAsync = function(filePath) {
-  // TODO
+  return new Promise((resolve, reject) => {
+    fs.readFile(filePath, 'utf8', (err, data) => {
+      if (err) { 
+        reject(err);
+      } else {
+        let result = data.split('\r')[0]
+        resolve(result);
+      }
+    })
+  })
 };
 
 // This function should retrieve the status code of a GET request to `url`
